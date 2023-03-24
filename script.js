d6777aa6c210de8ef26f6cfddca8c586764878b3
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-03-24 13:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-function calculateMinCost() {
-  //your code here
-var res = 0;
-	var cost = 0;
-	var inputArray = document.getElementById("rope-lengths").value;
+// Get the input element and the result element
+const input = document.querySelector('input[type="text"]');
+const result = document.querySelector('#result');
 
-	inputArray = inputArray.split(',').map(Number);
-	console.log(inputArray);
-	for(let i = 0;i< inputArray.length -1;i+=0) {
-		if (inputArray.length == 0) break;
-		inputArray = inputArray.sort((a,b) => b-a);
-		res = inputArray.pop()+ inputArray.pop();
-		cost += res;
-		inputArray[inputArray.length] = res; }
+// Function to calculate the minimum cost of connecting ropes
+function connectRopes(arr) {
+  // Create a min heap or priority queue of rope lengths
+  const pq = new MinPriorityQueue();
+  arr.forEach(len => pq.enqueue(len, len));
 
-	document.getElementById("result").innerHTML = cost;
-	}
-  
+  let cost = 0;
+  // Connect ropes until only one rope is left
+  while (pq.size() > 1) {
+    // Get the two smallest ropes
+    const rope1 = pq.dequeue().element;
+    const rope2 = pq.dequeue().element;
+
+    // Connect the ropes and add the cost
+    const newRope = rope1 + rope2;
+    cost += newRope;
+
+    // Add the new rope to the min heap
+    pq.enqueue(newRope, newRope);
+  }
+
+  // Return the total cost
+  return cost;
+}
+
+// Add a submit event listener to the input element
+input.addEventListener('submit', event => {
+  event.preventDefault();
+  // Get the rope lengths as an array of integers
+  const arr = input.value.split(',').map(Number);
+  // Calculate the minimum cost of connecting the ropes
+  const minCost = connectRopes(arr);
+  // Display the result
+  result.textContent = `Minimum cost: ${minCost}`;
+});
